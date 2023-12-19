8a50854c5a1d2fc7115f41d3ef63f6285f4de9ca
DharaniSenthil-commits <dharanieastsenthilkumar@gmail.com>
2023-12-19 12:35:06 +0530

    DSA Array

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 #DSA Zero to Mastery
-print("DSA Learning")
+
+#Array
+#An array is a powerful data structure that allows users to store and manipulate a collection of elements, all of the same data type in a single variable. Simply, it is a collection of elements of the same data type stored at contagious memory locations that can be randomly accessed with their index number
+
+import array
+arr=array.array("i",[1,2,3,4,5])
+print(arr)

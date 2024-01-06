46e95dd09875d13d5799ca14dece74389873f91b
DharaniSenthil-commits <dharanieastsenthilkumar@gmail.com>
2024-01-06 21:15:41 +0530

    Sum of two number

---
+++ AddTwoNo.py
@@ -0,0 +1,9 @@
+#Sum of two numbers
+
+Value1=int(input("Enter a value1 : "))
+Value2=int(input("Entet a value2 : "))
+
+Output=Value1 + Value2
+print("Result is : ",Output )
+
+print("Result in print statement : ",Value1+Value2)
